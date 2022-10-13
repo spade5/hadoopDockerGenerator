7e1881a25f18ac26818abb628d7018a5b6005320
spade5 <spade5@spade5.name>
2022-10-13 16:54:25 +0800

    init

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,19 +3,23 @@
 # hosts
 # cat /scripts/hosts >> /etc/hosts
 
-echo "HADOOP_HOME:"
-HADOOP_HOME=/usr/local/hadoop-3.3.4
-echo $HADOOP_HOME
-echo "export JAVA_HOME=/usr/local/jdk11" >> $HADOOP_HOME/etc/hadoop-env.sh
+if [[ -d "/scripts/confbak/" ]]; then
+    echo "hadoop is inited!"
+else
+    echo "HADOOP_HOME:"
+    HADOOP_HOME=/usr/local/hadoop-3.3.4
+    echo $HADOOP_HOME
+    echo "export JAVA_HOME=/usr/local/jdk11" >> $HADOOP_HOME/etc/hadoop/hadoop-env.sh
 
-mkdir /scripts/confbak
-cp /scripts/conf/* /scripts/confbak/
+    mkdir /scripts/confbak
+    cp /scripts/conf/* /scripts/confbak/
 
-mv -f /scripts/conf/core-site.xml $HADOOP_HOME/etc/hadoop/core-site.xml
-mv -f /scripts/conf/hdfs-site.xml $HADOOP_HOME/etc/hadoop/hdfs-site.xml
-mv -f /scripts/conf/mapred-site.xml $HADOOP_HOME/etc/hadoop/mapred-site.xml
-mv -f /scripts/conf/yarn-site.xml $HADOOP_HOME/etc/hadoop/yarn-site.xml
-mv -f /scripts/conf/workers $HADOOP_HOME/etc/hadoop/workers
+    mv -f /scripts/conf/core-site.xml $HADOOP_HOME/etc/hadoop/core-site.xml
+    mv -f /scripts/conf/hdfs-site.xml $HADOOP_HOME/etc/hadoop/hdfs-site.xml
+    mv -f /scripts/conf/mapred-site.xml $HADOOP_HOME/etc/hadoop/mapred-site.xml
+    mv -f /scripts/conf/yarn-site.xml $HADOOP_HOME/etc/hadoop/yarn-site.xml
+    mv -f /scripts/conf/workers $HADOOP_HOME/etc/hadoop/workers
+fi
 
 
 # scp -r $HADOOP_HOME/etc/hadoop/ hadoop1:$HADOOP_HOME/etc/hadoop/
